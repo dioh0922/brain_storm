df976f0053f5944b870888d68de3e26974d2cd76
dioh0922 <diohound@gmail.com>
2022-05-11 10:14:07 +0900

    フォームスタイル調整

--- view/index.blade.php
+++ view/index.blade.php
@@ -3,24 +3,25 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 		<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 		<!-- Compiled and minified CSS -->
-		    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 
-		    <!-- Compiled and minified JavaScript -->
-		    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+		<!-- Compiled and minified JavaScript -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/mermaid/8.14.0/mermaid.min.js" integrity="sha512-vLumCjg7NKEQKGM+xAgBYTvQ90DVu6Eo7vS1T/iPf2feNLcrpGxvumuUUmE3CPiCUPgviyKbtpWGDbhnVnmJxg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 		<title></title>
 
 		<script type="text/javascript">
-		$(document).ready(function(){
-    	$('.tabs').tabs();
-  	});
-		$(document).ready(function(){
-    	$('select').formSelect();
-  	});
-</script>
+			$(document).ready(function(){
+				$('.tabs').tabs();
+	  	});
+			$(document).ready(function(){
+				$('select').formSelect();
+			});
+		</script>
 
 	</head>
 	<body>
@@ -40,10 +41,10 @@
 		</div>
 		@endif
 		<div class="row" >
-			<div class="col s12">
-				<ul class="tabs">
+			<div class="col s7">
+				<ul class="tabs z-depth-1" style="height: 60px;">
 					@foreach($list as $theme)
-						<li class="tab col s1">
+						<li class="tab">
 							@if($id == $theme->id)
 								<a class="active" targt="_self" href="./?id={{$theme->id}}">{{$theme->discussion_title}}</a>
 							@else
@@ -55,32 +56,39 @@
 			</div>
 		</div>
 
-		<form class="" action="./api/addTheme.php" method="post">
-			<input type="text" name="title" placeholder="目的地を入力" value="">
-			<input type="submit" name="" value="テーマ追加">
-		</form>
+		<div class="green lighten-5">
+			<form class="" name="theme" action="./api/addTheme.php" method="post">
+				<input type="text" name="title" placeholder="目的地を入力" value="">
+				<a href="javascript:theme.submit()" class="btn-floating btn waves-effect waves-light red">
+					<i class="tiny material-icons">file_upload</i>
+				</a>
+			</form>
+		</div>
 
-		<form class="" action="./api/editNode.php" method="post">
-			<div class="input-field">
-				<select name="base">
-					<option value="0">派生元</option>
+		<div class="grey lighten-4">
+			<form class="" name="comment" action="./api/editNode.php" method="post">
+				<div class="input-field">
+					<select name="base">
+						<option value="0">派生元</option>
+						@foreach($node_item as $obj)
+							<option value='{{$obj["id"]}}'>{{$obj["text"]}}</option>
+						@endforeach
+					</select>
+				</div>
+				<input type="hidden" name="id" value="<?php echo $id ?>"/>
+				<input type="text" name="detail" value="" placeholder="行き方/所要時間etcを入力">
+				<select class="" name="dist">
+					<option value="0">コメント対象</option>
 					@foreach($node_item as $obj)
 						<option value='{{$obj["id"]}}'>{{$obj["text"]}}</option>
 					@endforeach
 				</select>
-			</div>
-			<input type="hidden" name="id" value="<?php echo $id ?>"/>
-			<input type="text" name="detail" value="" placeholder="行き方/所要時間etcを入力">
-			<select class="" name="dist">
-				<option value="0">コメント対象</option>
-				@foreach($node_item as $obj)
-					<option value='{{$obj["id"]}}'>{{$obj["text"]}}</option>
-				@endforeach
-			</select>
-			<input type="text" name="comment" value="" placeholder="場所を入力">
-			<input type="submit" value="アイデア追加"/>
-		</form>
+				<input type="text" name="comment" value="" placeholder="場所を入力">
+				<a href="javascript:comment.submit()" class="btn-floating btn waves-effect waves-light green">
+					<i class="tiny material-icons">edit</i>
+				</a>
+			</form>
+		</div>
 
-		<script src="./js/src/brain_storm.js"></script>
 	</body>
 </html>
