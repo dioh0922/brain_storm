93546142c7bd8369125484c29ca348f651925a0c
dioh0922 <diohound@gmail.com>
2022-05-29 09:52:00 +0900

    エラー表示

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 require_once(dirname(__FILE__)."/svr/BrainStormModule.php");
 $module = new BrainStorm();
 $id = 0;
+ini_set('display_errors', "On");
 if(array_key_exists("id", $_GET)){
 	$id = (int)$_GET["id"];
 }
